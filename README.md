3bc6c4d03c85cfb51df8a3c2525c09bba9571f07
Kosta <kostabulgakov89@gmail.com>
2018-11-17 13:44:49 -0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # windows-pe-signature-verifying
+
+//https://blogs.msdn.microsoft.com/winsdk/2013/08/07/using-winverifytrust-api-to-verify-the-embedded-signature-of-a-file-or-fall-back-to-catalog-signature-verification/
+//https://docs.microsoft.com/en-us/windows/desktop/seccrypto/certificate-and-trust-return-values
---
+++ tests/PeSignatureVerifierTests.cpp
@@ -0,0 +1,55 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "./../src/PeSignatureVerifier.h"
+
+TEST_CASE("Test common Windows system files", "[windows]")
+{
+
+	bool lResult = PeSignatureVerifier::VerifySignature(
+		L"C:\\Windows\\explorer.exe");
+
+	REQUIRE(lResult == true);
+
+	lResult = PeSignatureVerifier::VerifySignature(
+		L"C:\\Windows\\System32\\msi.dll");
+
+	REQUIRE(lResult == true);
+
+	lResult = PeSignatureVerifier::VerifySignature(
+		L"C:\\Windows\\System32\\Defrag.exe");
+
+	REQUIRE(lResult == true);
+
+	lResult = PeSignatureVerifier::VerifySignature(
+		L"C:\\Windows\\System32\\calc.exe");
+
+	REQUIRE(lResult == true);
+}
+
+TEST_CASE("Test common Program Files applications", "[ProgramFiles]")
+{
+
+	bool lResult = PeSignatureVerifier::VerifySignature(
+		L"C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+
+	REQUIRE(lResult == true);
+
+}
+
+TEST_CASE("Try to check if current running exe is signed", "[GetModuleFileName]")
+{
+	wchar_t lCurrentExePath[MAX_PATH];
+	GetModuleFileName(NULL, lCurrentExePath, MAX_PATH);
+
+	bool lResult = PeSignatureVerifier::VerifySignature(lCurrentExePath);
+
+	REQUIRE(lResult == false);
+}
+
+
+TEST_CASE("Try to scan file with invalid name", "[invalid]")
+{
+	bool lResult = PeSignatureVerifier::VerifySignature(L"INVALID_FILE_NAME");
+
+	REQUIRE(lResult == false);
+}
