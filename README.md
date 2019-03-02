739bc9d94ad44f0b10aa413966ddf5e7f4e9b136
Kosta <kostabulgakov89@gmail.com>
2019-03-02 04:30:55 -0800

    add documentation

--- README.md
+++ README.md
@@ -1,12 +1,43 @@
 # windows-pe-signature-verifying
 
 ## Brief
-Library that used to verify PE files signatures. 
+Library that used to verify PE files signatures and get certificate info
+
+## Projects
+**command-line-tool**  <br />
+**pe-signature-utils**  <br />
+**tests**  <br />
 
 ## Links
 [WinVerifyTrust](https://docs.microsoft.com/en-us/windows/desktop/api/wintrust/nf-wintrust-winverifytrust) <br />
 [Certificate and Trust return values](https://docs.microsoft.com/en-us/windows/desktop/seccrypto/certificate-and-trust-return-values)  <br />
 [Windows 8 and 10 signature hash algorithm](https://stackoverflow.com/questions/26216789/getting-digital-signature-from-mmc-exe-at-windows-8)  <br />
+[Forum discussion about PE signature info](http://qaru.site/questions/7338503/amended-code-to-retrieve-dual-signature-information-from-pe-executable-in-windows)  <br />
+
+
+## Usage
+
+### command-line-tool
+
+Example usage:  <br />
+&nbsp;&nbsp;`command-line-tool.exe "C:\\Program Files\\Mozilla Firefox\\firefox.exe"`
+
+Example output: <br />
+&nbsp;&nbsp;`File name: C:\Program Files\Mozilla Firefox\firefox.exe` <br />
+&nbsp;&nbsp;`Verified: Signed` <br />
+&nbsp;&nbsp;`SHA256: 7AF330A6446D56457BA9E90FFF0418A589E26385566BD7AF8F28578E3210C553` <br />
+&nbsp;&nbsp;`Serial number: 0c5396dcb2949c70fac48ab08a07338e` <br />
+&nbsp;&nbsp;`Issuer name: DigiCert SHA2 Assured ID Code Signing CA` <br />
+&nbsp;&nbsp;`Subject name: Mozilla Corporation` <br />
+&nbsp;&nbsp;`Signing algorithm: sha256RSA` <br />
+&nbsp;&nbsp;`Signing date: 08/01/2019 10:01` <br />
+
+### pe-signature-utils
+&nbsp;&nbsp;Generates static library.  <br />
+&nbsp;&nbsp;The public API is specified in **src/PeSignatureVerifier.h** header.<br />
+
+### tests
+&nbsp;&nbsp;Generates binary of tests for pe-signature-utils. <br />
 
 ## Todos
 Dual signatures
--- pe-signature-utils/src/PeSignatureVerifier.h
+++ pe-signature-utils/src/PeSignatureVerifier.h
@@ -1,12 +1,6 @@
-#ifndef __PE_SIGNATURE_VERIFIER_H
-#define __PE_SIGNATURE_VERIFIER_H
-
-#include <string>
-#include <iostream>
-#include <memory>
+#pragma once
 
 #include ".\certificate_info\SignerInfo.h"
-
 #include ".\native_api_wrappers\CryptoApiWrapper.h"
 #include ".\native_api_wrappers\HashApiWrapper.h"
 #include ".\native_api_wrappers\TrustVerifyWrapper.h"
@@ -20,33 +14,43 @@ public:
 
 	/**
 	* @brief: Check if the given file is signed with valid certificate.
-	* @returns: ERROR_SUCCESS iff the PE's signature is verified, othervise returns error code.
+	* @returns: ERROR_SUCCESS iff the PE's signature is verified, otherwise returns error code.
 	*/
-	static DWORD CheckFileSignature(std::wstring aPePath);
+	static DWORD CheckFileSignature(
+		__in std::wstring aPePath);
 
 	/**
+	* @brief: Calculate hash for a given file.
+	* 
+	* @param: aFileName - Full path of file for hash calculation.
+	* @param: aHashType - Hash algorithm to use. For example L"SHA256".
+	* @param: aHashWstr - Wstring containing the calculated hash.
+	*
+	* @returns: ERROR_SUCCESS iff call is successful, otherwise returns error code.
 	*
 	*/
 	static DWORD CalculateFileHash(
-		std::wstring aFileName,
-		std::wstring aHashType,
-		std::wstring& aHashWstr);
+		__in  std::wstring aFileName,
+		__in  std::wstring aHashType,
+		__out std::wstring& aHashWstr);
 
 	/**
+	* @brief: Get info about the certificate used to sign the file.
+	*
+	* @returns: ERROR_SUCCESS iff call is successful, otherwise returns error code.
 	*
 	*/
 	static DWORD GetCertificateInfo(
-		std::wstring aFileName,
-		SignerInfoPtr &aCertInfo);
+		__in  std::wstring aFileName,
+		__out SignerInfoPtr &aCertInfo);
 
 	/**
+	* @brief: Get info about the time stamp certificate used to sign the file.
+	*
+	* @returns: ERROR_SUCCESS iff call is successful, otherwise returns error code.
 	*
 	*/
 	static DWORD GetTimestampCertificateInfo(
 		std::wstring aFileName,
 		TimeStampCertInfoPtr &aCertInfo);
-
 };
-
-
-#endif
